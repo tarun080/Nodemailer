671f161df5810f5dfae4f200edc2c69e501463a3
tarun080 <84904266+tarun080@users.noreply.github.com>
2022-11-01 23:14:25 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (user) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+    });
+    return schema.validate(user);
+};
+
+module.exports = { User, validate };
